9a55a4b2795960e51e2917a834ebff6bbf739f14
Amnabi <77108078+Amnabi@users.noreply.github.com>
2021-04-13 22:22:54 +0900

    Add files via upload

---
+++ Source/HeavyMelee/ExtendedShield.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace HeavyMelee {
+    public class ExtendedShield : DefModExtension {
+    }
+}
