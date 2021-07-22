ae39685ed103bfe5afd65965f9dde1fbea1217bb
Joshua <legodude17@users.noreply.github.com>
2021-07-22 13:31:21 -0700

    NPCs can use but only in melee

--- Source/HeavyMelee/Verb_SelfExplode.cs
+++ Source/HeavyMelee/Verb_SelfExplode.cs
@@ -5,6 +5,17 @@ namespace HeavyMelee
 {
     public class Verb_SelfExplode : Verb
     {
+        private bool forced;
+
+        public override Verb GetVerb
+        {
+            get
+            {
+                forced = true;
+                return base.GetVerb;
+            }
+        }
+
         protected override bool TryCastShot()
         {
             DoExplode();
@@ -16,6 +27,15 @@ namespace HeavyMelee
             DamageWorker_Flame_30Degrees.ExplosionOriginator = CasterPawn;
             GenExplosion.DoExplosion(caster.Position, caster.Map, verbProps.range, verbProps.meleeDamageDef, caster,
                 verbProps.meleeDamageBaseAmount, ignoredThings: new List<Thing> {caster});
+        }
+
+        public override bool Available()
+        {
+            if (!base.Available()) return false;
+            if (!forced)
+                return CasterIsPawn && (CasterPawn.mindState?.enemyTarget?.Position.AdjacentTo8WayOrInside(Caster.Position) ?? false);
+            forced = false;
+            return true;
         }
     }
 }
